fc8dc7a8b725eaec4c89c895206245241989ea34
Srijanee-Ghosh <srijaneeghosh1@gmail.com>
2022-09-26 13:37:36 +0530

    Add project files.

---
+++ ECommerceApp/Interface/IProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerceApp.Models;
+
+namespace ECommerceApp.Interface
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Category>> GetCategories();
+
+        Task<IEnumerable<Product>> GetProducts();
+
+        Task<ProductViewModel> GetProduct(int? ProductId);
+
+        Task<int> AddCategory(Category cat);
+
+        Task<int> AddProduct(Product prod);
+
+        //Task<int> DeleteCategory(int? CategoryId);
+
+        Task<int> DeleteProduct(int? ProductId);
+
+        Task UpdateProduct(Product prod);
+    }
+}
---
+++ ECommerceApp/Startup.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerceApp.Models;
+using ECommerceApp.Repository;
+using ECommerceApp.Interface;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ECommerceApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddDbContext<EcommerceDBContext>(item => item.UseSqlServer(Configuration.GetConnectionString("DBConn")));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
